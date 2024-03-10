8c16cf7ed11f7a9292794695d399af552bc86070
Brenda <brentorres26@gmail.com>
2024-03-10 18:21:08 -0300

    cambios de estilo

--- app.js
+++ app.js
@@ -4,11 +4,12 @@ const cerrar = document.querySelector("#cerrar");
 
 abrir.addEventListener("click", () => {
     nav.classList.add("visible");
-})
+});
 
 cerrar.addEventListener("click", () => {
     nav.classList.remove("visible");
-})
+});
+
 
 const form = document.getElementById("form");
 const nombre = document.getElementById("nombre");
